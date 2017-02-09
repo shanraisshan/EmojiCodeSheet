054b2c8e9e8c6782c28575b7d0dafcc9067be31b
Shayan Rais <shanraisshan@gmail.com>
2017-02-09 11:41:11 +0500

    [language] Android Version-2 updated

---
+++ android/string/People2.java
@@ -0,0 +1,18 @@
+/**
+ * Shayan Rais (http://shanraisshan.com)
+ * created on 2/9/2017
+ */
+public class People2 {
+
+    //Row#: 1   
+    public static final String GRINNING_FACE = "😀"; //http://emojipedia.org/grinning-face/
+    public static final String SMILING_FACE_WITH_OPEN_MOUTH = "😃"; //emojipedia.org/smiling-face-with-open-mouth/
+    public static final String SMILING_FACE_WITH_OPEN_MOUTH_SMILE_EYES = "😄"; //emojipedia.org/smiling-face-with-open-mouth-and-smiling-eyes/
+    public static final String GRINNING_FACE_WITH_SMILE_EYES = "😁"; //emojipedia.org/grinning-face-with-smiling-eyes/
+    public static final String SMILING_FACE_WITH_OPEN_MOUTH_CLOSE_EYES = "😆"; //emojipedia.org/smiling-face-with-open-mouth-and-tightly-closed-eyes/
+    public static final String SMILING_FACE_WITH_OPEN_MOUTH_COLD_SWEAT = "😅"; //emojipedia.org/smiling-face-with-open-mouth-and-cold-sweat/
+    public static final String FACE_WITH_TEARS_OF_JOY = "😂"; //emojipedia.org/face-with-tears-of-joy/
+    public static final String ROLLING_ON_THE_FLOOR_LAUGHING = "🤣"; //emojipedia.org/rolling-on-the-floor-laughing/
+
+ 
+}
