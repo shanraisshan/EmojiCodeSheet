fab4b73913efdecf9e036c8c257f44747bc2384d
Shayan Rais <shanraisshan@gmail.com>
2017-02-09 11:16:26 +0500

    [language] Android Version-2 updated

---
+++ android/codepoint/People2.java
@@ -0,0 +1,18 @@
+/**
+ * Shayan Rais (http://shanraisshan.com)
+ * created on 2/9/2017
+ */
+public class People2 {
+
+    //Row#: 1
+    public static final int GRINNING_FACE = 0x1f600; //https://www.emojibase.com/emoji/1f600/grinningface
+    public static final int SMILING_FACE_WITH_OPEN_MOUTH = 0x1f603; //https://www.emojibase.com/emoji/1f603/smilingfacewithopenmouth
+    public static final int SMILING_FACE_WITH_OPEN_MOUTH_SMILE_EYES = 0x1f604; //https://www.emojibase.com/emoji/1f604/smilingfacewithopenmouthandsmilingeyes
+    public static final int GRINNING_FACE_WITH_SMILE_EYES = 0x1f601; //https://www.emojibase.com/emoji/1f601/grinningfacewithsmilingeyes
+    public static final int SMILING_FACE_WITH_OPEN_MOUTH_CLOSE_EYES = 0x1f606; //https://www.emojibase.com/emoji/1f606/smilingfacewithopenmouthandtightlyclosedeyes
+    public static final int SMILING_FACE_WITH_OPEN_MOUTH_COLD_SWEAT = 0x1f605; //https://www.emojibase.com/emoji/1f605/smilingfacewithopenmouthandcoldsweat
+    public static final int FACE_WITH_TEARS_OF_JOY = 0x1f602; //https://www.emojibase.com/emoji/1f602/facewithtearsofjoy
+    public static final int ROLLING_ON_THE_FLOOR_LAUGHING = 0x1f923; //http://www.emojipedia.org/rolling-on-the-floor-laughing/
+
+ 
+}
