8bd65bce1859409b8bdb4422f4f2f4ccb180b343
Shayan Rais <shanraisshan@gmail.com>
2017-02-10 10:15:15 +0500

    [language] Android Version-2 updated

--- android/codepoint/People2.java
+++ android/codepoint/People2.java
@@ -14,5 +14,14 @@ public class People2 {
     public static final int FACE_WITH_TEARS_OF_JOY = 0x1f602; //https://www.emojibase.com/emoji/1f602/facewithtearsofjoy
     public static final int ROLLING_ON_THE_FLOOR_LAUGHING = 0x1f923; //http://www.emojipedia.org/rolling-on-the-floor-laughing/
 
- 
+ 	//Row#: 2
+ 	public static final char WHITE_SMILING_FACE = 0x263a; //https://www.emojibase.com/emoji/263a/whitesmilingface
+ 	public static final int SMILING_FACE_WITH_SMILE_EYES = 0x1f60a; //https://www.emojibase.com/emoji/1f60a/smilingfacewithsmilingeyes
+ 	public static final int SMILING_FACE_WITH_HALO = 0x1f607; //https://www.emojibase.com/emoji/1f607/smilingfacewithhalo
+ 	public static final int SLIGHTLY_SMILING_FACE = 0x1f642; //https://www.emojibase.com/emoji/1f642/slightlysmilingface
+    public static final int UPSIDE_DOWN_FACE = 0x1f643; //http://emojipedia.org/upside-down-face/
+    public static final int WINKING_FACE = 0x1f609; //https://www.emojibase.com/emoji/1f609/winkingface
+    public static final int RELIEVED_FACE = 0x1f60c; //https://www.emojibase.com/emoji/1f60c/relievedface
+    public static final int SMILING_FACE_WITH_HEART_EYES = 0x1f60d; //https://www.emojibase.com/emoji/1f60d/smilingfacewithheartshapedeyes
+
 }
