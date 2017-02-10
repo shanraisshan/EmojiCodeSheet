df6c7a9cedf958cb56de8ce343d0e04cc1fb436c
Shayan Rais <shanraisshan@gmail.com>
2017-02-10 10:30:45 +0500

    [language] Android Version-2 updated

--- android/string/People2.java
+++ android/string/People2.java
@@ -6,13 +6,22 @@ public class People2 {
 
     //Row#: 1   
     public static final String GRINNING_FACE = "😀"; //http://emojipedia.org/grinning-face/
-    public static final String SMILING_FACE_WITH_OPEN_MOUTH = "😃"; //emojipedia.org/smiling-face-with-open-mouth/
-    public static final String SMILING_FACE_WITH_OPEN_MOUTH_SMILE_EYES = "😄"; //emojipedia.org/smiling-face-with-open-mouth-and-smiling-eyes/
-    public static final String GRINNING_FACE_WITH_SMILE_EYES = "😁"; //emojipedia.org/grinning-face-with-smiling-eyes/
-    public static final String SMILING_FACE_WITH_OPEN_MOUTH_CLOSE_EYES = "😆"; //emojipedia.org/smiling-face-with-open-mouth-and-tightly-closed-eyes/
-    public static final String SMILING_FACE_WITH_OPEN_MOUTH_COLD_SWEAT = "😅"; //emojipedia.org/smiling-face-with-open-mouth-and-cold-sweat/
-    public static final String FACE_WITH_TEARS_OF_JOY = "😂"; //emojipedia.org/face-with-tears-of-joy/
-    public static final String ROLLING_ON_THE_FLOOR_LAUGHING = "🤣"; //emojipedia.org/rolling-on-the-floor-laughing/
+    public static final String SMILING_FACE_WITH_OPEN_MOUTH = "😃"; //http://emojipedia.org/smiling-face-with-open-mouth/
+    public static final String SMILING_FACE_WITH_OPEN_MOUTH_SMILE_EYES = "😄"; //http://emojipedia.org/smiling-face-with-open-mouth-and-smiling-eyes/
+    public static final String GRINNING_FACE_WITH_SMILE_EYES = "😁"; //http://emojipedia.org/grinning-face-with-smiling-eyes/
+    public static final String SMILING_FACE_WITH_OPEN_MOUTH_CLOSE_EYES = "😆"; //http://emojipedia.org/smiling-face-with-open-mouth-and-tightly-closed-eyes/
+    public static final String SMILING_FACE_WITH_OPEN_MOUTH_COLD_SWEAT = "😅"; //http://emojipedia.org/smiling-face-with-open-mouth-and-cold-sweat/
+    public static final String FACE_WITH_TEARS_OF_JOY = "😂"; //http://emojipedia.org/face-with-tears-of-joy/
+    public static final String ROLLING_ON_THE_FLOOR_LAUGHING = "🤣"; //http://emojipedia.org/rolling-on-the-floor-laughing/
 
- 
+ 	//Row#: 2
+ 	public static final String WHITE_SMILING_FACE = "☺"; //http://emojipedia.org/white-smiling-face/
+ 	public static final String SMILING_FACE_WITH_SMILE_EYES = "😊"; //http://emojipedia.org/smiling-face-with-smiling-eyes/
+ 	public static final String SMILING_FACE_WITH_HALO = "😇"; //http://emojipedia.org/smiling-face-with-halo/
+ 	public static final String SLIGHTLY_SMILING_FACE = "🙂"; //http://emojipedia.org/slightly-smiling-face/
+    public static final String UPSIDE_DOWN_FACE = "🙃"; //http://emojipedia.org/upside-down-face/
+    public static final String WINKING_FACE = "😉"; //http://emojipedia.org/winking-face/
+    public static final String RELIEVED_FACE = "😌"; //http://emojipedia.org/relieved-face/
+    public static final String SMILING_FACE_WITH_HEART_EYES = "😍"; //http://emojipedia.org/smiling-face-with-heart-shaped-eyes/
+
 }
